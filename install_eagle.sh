a45f68a635286b91563c79e4ae61bd116b80b79d
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2025-04-13 08:51:39 +0530

    Conditional roles.

--- install_eagle.sh
+++ install_eagle.sh
@@ -3,23 +3,32 @@ export DHCP_ROUTER=192.168.86.1
 export DHCP_START=192.168.86.50
 export DHCP_END=192.168.86.250
 export NEXTCLOUD_ADMIN_USER=srivatsan
+export DNS_UPSTREAM=8.8.8.8,8.8.4.4
 
-if [[ -z "${PI_HOLE_ADMIN_PASSWD}" ]]; then
+export CONFIGURE_BASIC="true"
+export INSTALL_HTTP="true"
+export INSTALL_MYSQL="true"
+export INSTALL_PIHOLE="true"
+export INSTALL_NEXTCLOUD="true"
+export INSTALL_JELLYFIN="true"
+export INSTALL_GITHUB_RUNNER="true"
+
+if [[ "${INSTALL_PIHOLE}" == "true" && -z "${PI_HOLE_ADMIN_PASSWD}" ]]; then
   echo "Need PiHole password" 1>&2
   exit 1
 fi
 
-if [[ -z "${NEXTCLOUD_DB_PASSWD}" ]]; then
+if [[ "${INSTALL_NEXTCLOUD}" == "true" && -z "${NEXTCLOUD_DB_PASSWD}" ]]; then
   echo "Need Database password" 1>&2
   exit 1
 fi
 
-if [[ -z "${NEXTCLOUD_ADMIN_PASSWD}" ]]; then
+if [[ "${INSTALL_NEXTCLOUD}" == "true" && -z "${NEXTCLOUD_ADMIN_PASSWD}" ]]; then
   echo "Need NextCloud Admin password" 1>&2
   exit 1
 fi
 
-if [[ -z "${GITHUB_TOKEN}" ]]; then
+if [[ "${INSTALL_GITHUB_RUNNER}" == "true" && -z "${GITHUB_TOKEN}" ]]; then
   echo "Need GitHub Runner token" 1>&2
   exit 1
 fi
