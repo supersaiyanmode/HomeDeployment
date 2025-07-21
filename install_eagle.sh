b82c4943fb0ed66403001ad31ed477d57833bc33
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2025-07-21 22:21:14 +0530

    Move params to hosts.yml.

--- install_eagle.sh
+++ install_eagle.sh
@@ -1,21 +1,17 @@
 export MAIN_HOST_NAME=eagle.com
 export SUBNET="192.168.86.0/24"
-export DHCP_ROUTER=192.168.86.1
-export DHCP_START=192.168.86.50
-export DHCP_END=192.168.86.250
-export NEXTCLOUD_ADMIN_USER=srivatsan
-export DNS_UPSTREAM=8.8.8.8,8.8.4.4
-export CUSTOM_HOSTS="192.168.86.2 pihole.eagle.com;192.168.86.2 nextcloud.eagle.com;192.168.86.2 jellyfin.eagle.com;192.168.86.2 wud.eagle.com"
 
-export CONFIGURE_BASIC="true"
-export INSTALL_HTTP="true"
-export INSTALL_MYSQL="true"
+export CONFIGURE_BASIC="false"
+export CONFIGURE_GPIO="false"
+export INSTALL_HTTP="false"
+export INSTALL_MYSQL="false"
 export INSTALL_PIHOLE="true"
-export INSTALL_NEXTCLOUD="true"
-export INSTALL_JELLYFIN="true"
-export INSTALL_GITHUB_RUNNER="true"
-export INSTALL_WUD="true"
-export INSTALL_TAILSCALE="true"
+export INSTALL_NEXTCLOUD="false"
+export INSTALL_JELLYFIN="false"
+export INSTALL_GITHUB_RUNNER="false"
+export INSTALL_WUD="false"
+export INSTALL_TAILSCALE="false"
+export INSTALL_QBITTORRENT="true"
 
 if [[ "${INSTALL_PIHOLE}" == "true" && -z "${PI_HOLE_ADMIN_PASSWD}" ]]; then
   echo "Need PiHole password" 1>&2
@@ -42,4 +38,4 @@ if [[ "${INSTALL_TAILSCALE}" == "true" && -z "${TAILSCALE_AUTH_KEY}" ]]; then
   exit 1
 fi
 
-ansible-playbook -i $1, playbook.yml
+ansible-playbook -i hosts.yml playbook.yml
