29100f00ac8552406cd3b557517575e1ac2aafda
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2025-09-17 23:42:40 +0530

    Add Forgejo role.

--- install_eagle.sh
+++ install_eagle.sh
@@ -5,13 +5,14 @@ export CONFIGURE_BASIC="false"
 export CONFIGURE_GPIO="false"
 export INSTALL_HTTP="false"
 export INSTALL_MYSQL="false"
-export INSTALL_PIHOLE="true"
+export INSTALL_PIHOLE="false"
 export INSTALL_NEXTCLOUD="false"
 export INSTALL_JELLYFIN="false"
 export INSTALL_GITHUB_RUNNER="false"
 export INSTALL_WUD="false"
 export INSTALL_TAILSCALE="false"
-export INSTALL_QBITTORRENT="true"
+export INSTALL_QBITTORRENT="false"
+export INSTALL_FORGEJO="false"
 
 if [[ "${INSTALL_PIHOLE}" == "true" && -z "${PI_HOLE_ADMIN_PASSWD}" ]]; then
   echo "Need PiHole password" 1>&2
@@ -35,6 +36,11 @@ fi
 
 if [[ "${INSTALL_TAILSCALE}" == "true" && -z "${TAILSCALE_AUTH_KEY}" ]]; then
   echo "Need TailScale Auth Key. See: https://login.tailscale.com/admin/settings/keys " 1>&2
+  exit 1
+fi
+
+if [[ "${INSTALL_FORGEJO}" == "true" && -z "${FORGEJO_DB_PASSWD}" ]]; then
+  echo "Need Database password" 1>&2
   exit 1
 fi
 
